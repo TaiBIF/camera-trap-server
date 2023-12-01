f730d9b7c8bc527e52901aecc45c56b7c5d6fa1a
moogoo78 <moogoo78@gmail.com>
2023-12-01 16:58:04 +0800

    fix: contact us email cannot sent

--- static/js/contact_us.js
+++ static/js/contact_us.js
@@ -62,12 +62,13 @@ $(document).ready(function() {
   $(document).on("submit", "form", function(event) {
     $('.loading-pop').removeClass('d-none');
     event.preventDefault();
-    
+
+    window.f_data = new FormData();
     let otherFormData = new FormData($("#submitForm")[0]);
     for (var pair of otherFormData.entries()) {
       window.f_data.append(pair[0], pair[1]);
     }
-    
+
     $.ajax({
       url: $(this).attr("action"),
       type: $(this).attr("method"),
@@ -78,12 +79,12 @@ $(document).ready(function() {
       success: function (data, status) {
         $('.loading-pop').addClass('d-none');
         alert('請求已送出');
-        window.f_data = new FormData();
+        //window.f_data = new FormData();
       },
       error: function (xhr, desc, err) {
         $('.loading-pop').addClass('d-none');
         alert('未知錯誤，請聯繫管理員');
-        window.f_data = new FormData();
+        //window.f_data = new FormData();
       }
     });
   });
