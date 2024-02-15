1a56abbc94ac24ca701ac354a7d641427e556c90
MooGoo <moogoo78@gmail.com>
2024-02-15 18:32:24 +0800

    fix: revert check uploader available version

--- taicat/client_views.py
+++ taicat/client_views.py
@@ -234,20 +234,19 @@ def post_image_annotation1_1(request):
                 data = json.loads(json_file.read())
         #data = json.loads(request.body)
 
-        is_available = False
-        upload_key = data.get('key', '')
-        if upload_key:
-            keys = upload_key.split('/')
-            for available_version in settings.AVAILABLE_CLIENT_VERSIONS:
-                if available_version in keys[2]:
-                    is_available = True
+        #is_available = False
+        #upload_key = data.get('key', '')
+        #if upload_key:
+        #    keys = upload_key.split('/')
+        #    for available_version in settings.AVAILABLE_CLIENT_VERSIONS:
+        #        if available_version in keys[2]:
+        #            is_available = True
 
-        if is_available == False:
-            ret['error'] = f'目前上傳界面版本已經淘汰: {keys[2]}，無法上傳，請更新至最新版本'
-            ilog = InfoLog(name=upload_key, value=data['deployment_id'])
-            ilog.save()
-
-            return JsonResponse(ret)
+        #if is_available == False:
+        #    ret['error'] = f'目前上傳界面版本已經淘汰: {keys[2]}，無法上傳，請更新至最新版本'
+        #    ilog = InfoLog(name=upload_key, value=data['deployment_id'])
+        #    ilog.save()
+        #    return JsonResponse(ret)
 
         if deployment := Deployment.objects.get(pk=data['deployment_id']):
             # create or update DeploymentJournal
