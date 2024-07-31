4e85c50cf7a70d21322a0dda05f7179d65724cb0
JJJHANG <jangzhu5678@gmail.com>
2024-07-31 10:28:04 +0800

    update: send mail function for modified images

--- cron_scripts/check_modified_image.py
+++ cron_scripts/check_modified_image.py
@@ -6,10 +6,10 @@ from django.conf import settings
 from taicat.models import ModifiedImage
 from taicat.models import Contact
 from taicat.utils import get_studyarea_member
+from django.core.mail import send_mail
 
 modified_images = ModifiedImage.objects.all().values()
 df = pd.DataFrame(list(modified_images))
-print(df)
 
 modified_images = ModifiedImage.objects.all().values()
 df = pd.DataFrame(list(modified_images))
@@ -24,6 +24,8 @@ one_week_ago = today - datetime.timedelta(weeks=1)
 df2 = df[(df['last_updated'] >= one_week_ago) & (df['last_updated'] <= today)]
 # print(df2)
 
+email_subject = '[臺灣自動相機資訊系統] | 影像資料修改通知'
+
 groups = df2.groupby(['project_id', 'studyarea_id'])
 for (project_id, studyarea_id), group in groups:
     studyarea_memebers = get_studyarea_member(project_id, studyarea_id)
@@ -32,5 +34,8 @@ for (project_id, studyarea_id), group in groups:
     save_root = Path(settings.MEDIA_ROOT, 'email-attachment')
     save_root.mkdir(parents=True, exist_ok=True)  # 檢查並創建目錄
     save_path = os.path.join(save_root, f'{project_id}_{studyarea_id}.csv')
-    file_name = f'{project_id}_{studyarea_id}_{today}.csv'
-    group.to_csv(file_name)
+    group.to_csv(save_path)
+
+    download_url = f'https://staging.camera-trap.tw/media/email-attachment/{project_id}_{studyarea_id}.csv'
+    email_body = f'您所負責的樣區位置中的影像內容在上禮拜有進行修改，詳細的修改內容請點擊以下連結下載：{download_url}'
+    send_mail(email_subject, email_body, settings.CT_SERVICE_EMAIL, email_list)
